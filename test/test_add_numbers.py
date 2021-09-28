bca36870e71eaeba30fc78b2fd69988cfffc7716
Zach Cobell <zcobell@gmail.com>
2021-09-28 13:28:09 -0600

    fixing comments

--- test/test_add_numbers.py
+++ test/test_add_numbers.py
@@ -2,8 +2,18 @@
 
 from src.add_numbers import add_numbers
 
+
 def test_add_two_numbers():
-    assert add_numbers(3,5) == 8
+    assert add_numbers(3, 5) == 8
+
 
 def test_add_three_numbers():
-    assert add_numbers(2,3,4) == 9
+    assert add_numbers(2, 3, 4) == 9
+
+
+def test_add_four_numbers():
+    assert add_numbers(2, 3, 4, 5) == 14
+
+
+def test_add_five_numbers():
+    assert add_numbers(2, 3, 4, 5, 6) == 20
