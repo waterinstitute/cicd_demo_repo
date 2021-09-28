590c6d721ffd3a02e8b2f955d4a570a46e936fb2
Zach Cobell <zcobell@users.noreply.github.com>
2021-09-28 13:31:12 -0600

    Adding code to add two numbers (#1)
    * Adding number additions with tests
    
    * Removing toml for packaging
    
    * fixing comments

---
+++ src/add_numbers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+def add_numbers(a, b, c=0, d=0, e=0):
+    """
+    Adds some numbers
+    :param a: number 1
+    :param b:
+    :param c:
+    :param d:
+    :param e:
+    :return:
+    """
+    return a + b + c + d + e
+
